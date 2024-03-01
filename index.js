0d5d857fcd67ade17aa90fc7cdb020c8510f9b73
luvsense <yvsh7.btw@gmail.com>
2024-03-01 21:13:54 +0530

    security fix

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.once('ready', () => {
-    console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('messageCreate', message => {
-    if (message.content === 'ping') {
-        message.channel.send('Pong!');
-    }
+client.on("messageCreate", (message) => {
+  if (message.content === "ping") {
+    message.channel.send("Pong!");
+  }
 });
 
-client.login('MTIwMjIzMzA3OTQ5NDYxMDk1NA.GsYCjW.9Vhfb-WjosZ9wKlZGFesucwBmKugXADHsRrq74');
+client.login(token);
